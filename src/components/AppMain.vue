05c13c557a1db2c692566285d2cc299136e10323
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-20 09:27:11 +0200

    Creata struttura Main sez.2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -99,6 +99,61 @@ export default {
         </div>
       </div>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 px-3">
+          <h6 class="text-start">
+            POPULAR POSTS
+          </h6>
+          <div>
+            <img class="size" src="/public/img/blog-55.jpg" alt="blog-55">
+
+            <div>
+              <p>
+
+              </p>
+              <p>
+
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 px-3">
+          <h6 class="text-start">
+            RECENT POSTS
+          </h6>
+          <div>
+            <img class="size" src="/public/img/blog-65.jpg" alt="blog-65">
+
+            <div>
+              <p>
+
+              </p>
+              <p>
+
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 px-3">
+          <h6 class="text-start">
+            FEATURED POSTS
+          </h6>
+          <div>
+            <img class="size" src="" alt="">
+
+            <div>
+              <p>
+
+              </p>
+              <p>
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -147,8 +202,25 @@ main {
   
               font-weight: bold;
               color: $whiteColor;
-              width: 75%;
+              width: 88%;
             }
+          }
+        }
+      }
+    }
+  }
+  .container {
+
+    .row {
+
+      .col-4 {
+
+        div {
+
+          display: flex;
+          .size {
+
+            width: 50px;
           }
         }
       }
