9cac5cd03918d730c506e1124e99ab4e0f37c592
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-20 11:49:44 +0200

    Fatte prime 2 parti di SezTwoMain

--- src/components/SezTwoMain.vue
+++ src/components/SezTwoMain.vue
@@ -14,34 +14,130 @@ export default {
           <h6 class="text-start">
             POPULAR POSTS
           </h6>
-          <div>
+          <div class="py-2">
             <img class="size" src="/public/img/blog-55.jpg" alt="blog-55">
 
-            <div>
+            <section>
               <p>
-
+                Simple Ways to Have a Pretty Face
               </p>
               <p>
-
+                January 12, 2019
               </p>
-            </div>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-56.jpg" alt="blog-56">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-57.jpg" alt="blog-57">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-58.jpg" alt="blog-58">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-59.jpg" alt="blog-59">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
           </div>
         </div>
         <div class="col-4 px-3">
           <h6 class="text-start">
             RECENT POSTS
           </h6>
-          <div>
+          <div class="py-2">
             <img class="size" src="/public/img/blog-65.jpg" alt="blog-65">
 
-            <div>
+            <section>
               <p>
-
+                Simple Ways to Have a Pretty Face
               </p>
               <p>
-
+                January 12, 2019
               </p>
-            </div>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-66 (1).jpg" alt="blog-66">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-67.jpg" alt="blog-67">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-68.jpg" alt="blog-68">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
+          </div>
+          <div class="py-2">
+            <img class="size" src="/public/img/blog-69.jpg" alt="blog-69">
+
+            <section>
+              <p>
+                Simple Ways to Have a Pretty Face
+              </p>
+              <p>
+                January 12, 2019
+              </p>
+            </section>
           </div>
         </div>
         <div class="col-4 px-3">
@@ -51,14 +147,14 @@ export default {
           <div>
             <img class="size" src="" alt="">
 
-            <div>
+            <section>
               <p>
 
               </p>
               <p>
 
               </p>
-            </div>
+            </section>
           </div>
         </div>
       </div>
@@ -80,6 +176,20 @@ export default {
       .size {
 
         width: 50px;
+        height: 50px;
+      }
+
+      p {
+
+        text-align: start;
+        margin: 0;
+        padding-left: 10px;
+
+        &:last-child {
+
+          color: grey;
+          @include font-size-rule;
+        }
       }
     }
   }
